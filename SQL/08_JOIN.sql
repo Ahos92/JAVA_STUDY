42403176c412f0fbe5d080d6b40a580cfe2e0d58
Y H Kim <ahosism@gmail.com>
2020-11-18 17:49:55 +0900

    11/18_F

--- SQL/08_JOIN.sql
+++ SQL/08_JOIN.sql
@@ -90,4 +90,84 @@ WHERE
     AND job_title LIKE '%Marketing%';
     
 -- 테이블을 적절하게 잘 쪼개놓으면 필요할때 하나의 테이블로 합쳐서 사용할 수 있고
--- 데이터양을 줄일 수 있다 (대신 약간 느림)
+-- 데이터양을 줄일 수 있다 (대신 조회시 약간 느림)
+
+/*
+    # SELF JOIN
+        - 하나의 테이블 내에서 자신과 JOIN하여 원하는 데이터를 얻어내는 것
+        - manager_id 는 employees 테이블의 기본키만 들어갈 수 있는 외래키이다.
+*/
+-- 직원 이름과 해당 직원을 담당하는 매니저 이름 조회
+SELECT 
+    a.last_name AS "직원 이름", b.last_name AS "매니저 이름" 
+FROM 
+    employees a, employees b
+WHERE
+    a.manager_id = b.employee_id
+ORDER BY
+    a.last_name ASC;
+    
+-- 연습문제05 : 매니저가 King인 사원들의 이름과 job_title을 조회해보세요.
+SELECT 
+    a.last_name, job_title
+FROM 
+    employees a, employees b, jobs
+WHERE 
+    a.manager_id = b.employee_id
+    AND a.job_id = jobs.job_id
+    AND b.last_name = 'King';
+    
+-- 연습문제06 : Taylor 와 동일한 직책을 가진 사원의 정보를 모두 조회해보세요.
+SELECT
+    a.*
+FROM 
+    employees a, employees b
+WHERE 
+    a.job_id = b.job_id
+    AND b.last_name = 'Taylor';
+
+
+/*
+    # OUTER JOIN
+        - JOIN 조건을 만족하지 못해서 등장하지 못한 데이터들을 확인할 때 사용하는 JOIN
+        - 등장하지 못한 행을 확인하고 싶은 쪽의 반대편 조건에 (+)를 붙인다
+        - (+)를 붙인쪽의 행에 null을 추가하여 행을 확인한다.
+*/
+SELECT 
+    a.last_name as "내 이름", b.last_name "매니저 이름"
+FROM
+    employees a, employees b
+WHERE 
+    a.manager_id = b.employee_id(+)
+ORDER BY
+    b.last_name DESC;
+    
+-- 연습문제07 : 사원명/부서번호/부서이름을 출력 하되 사원이 한명도 속하지 않은 부서를 조회해보세요.
+SELECT
+    last_name, e.department_id, department_name
+FROM
+    employees e, departments d
+WHERE
+    e.department_id(+) = d.department_id
+ORDER BY
+    e.department_id;
+    
+-- 연습문제08 : 사원명/직급ID/직급명을 출력하되 사원이 한명도 속하지 않은 직급도 함께 조회해보세요.
+SELECT
+    last_name, e.job_id, job_title
+FROM
+    employees e, jobs j
+WHERE
+    e.job_id(+) = j.job_id
+ORDER BY
+    e.job_id;
+    
+-- 연습문제09 : 부서명/주소/도시명을 출력하되 소속된 부서가 없는 도시도 조회해보세요.
+SELECT 
+    department_name, street_address, city
+FROM
+    departments d, locations l
+WHERE
+    d.location_id(+) = l.location_id
+ORDER BY
+    d.department_name;
